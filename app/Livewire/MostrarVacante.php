098b5da990b8d3e9596a7b916083d726963d92be
Francisco J Casado <francasadomerino@gmail.com>
2023-11-06 10:48:31 +0100

    Postulacion doble evitada
    Las postulaciones dobles ya no se pueden realizar, en mostrarVacante se
    busca si hay una postulacion y el resultado se pasa a la vista que con
    un IF muestra o no el componente de postulacion

--- app/Livewire/MostrarVacante.php
+++ app/Livewire/MostrarVacante.php
@@ -7,8 +7,14 @@ use Livewire\Component;
 class MostrarVacante extends Component
 {
     public $vacante;
+    public $devAplicado;
     public function render()
-    {
+    {   
+        if(!$this->vacante->candidatos->where('user_id',auth()->user()->id)->all()){
+            $this->devAplicado = false;
+        } else {
+            $this->devAplicado = true;
+        }
         return view('livewire.mostrar-vacante');
     }
 }
--- app/Models/Candidato.php
+++ app/Models/Candidato.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Candidato extends Model
 {
@@ -16,7 +17,7 @@ class Candidato extends Model
     'vista'
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
